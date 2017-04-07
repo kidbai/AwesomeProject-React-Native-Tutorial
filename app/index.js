d54063e9d1526bf8e3aa6f330b7f364a5b530328
kidbai <ybooty0@gmail.com>
2017-04-07 14:12:56 +0800

    UPDATE:fetch

--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import { AppRegistry, ListView, Text, View } from 'react-native'
+import React from 'react'
+import {
+  AppRegistry,
+  Text,
+  View,
+  ListView,
+  Button,
+  TouchableHighlight
+} from 'react-native'
 import { getCnodeTopics } from '../api'
+import { StackNavigator } from 'react-navigation'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-class App extends Component {
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
   constructor(props) {
     super(props)
 
     this.state = {
-      dataSource: ds
+      topics: ds
     }
   }
   fetchData() {
@@ -16,26 +28,54 @@ class App extends Component {
       .then((res) => res.json())
       .then((res) => {
         this.setState({
-          dataSource: ds.cloneWithRows(res.data)
+          topics: ds.cloneWithRows(res.data)
         })
       })
       .catch((e) => {
-        alert(e)
+        console.error(e)
       })
   }
   componentWillMount() {
     this.fetchData()
   }
-  render() {
+  _renderRow(rowData) {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={{flex: 1, paddingTop: 42}}>
+      <Text onPress={ () => navigate('chat')}>{ rowData.title ? rowData.title : null}</Text>
+    )
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
         <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.title ? rowData.title : 'null'}</Text>}
+          dataSource={ this.state.topics }
+          renderRow={ (rowData) =>
+            this._renderRow(rowData)
+          }
         />
       </View>
     )
   }
 }
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: ({ state }) => `Chat with ${state.params.topic}`,
+  };
+  render() {
+    const { params } = this.props.navigation.state
+    return (
+      <View>
+        <Text>Chat with { params.topic }</Text>
+      </View>
+    );
+  }
+}
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
 
 export default App
