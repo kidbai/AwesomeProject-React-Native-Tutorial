1578c58bb6919340562dcab9d8a5113a499aec90
kidbai <ybooty0@gmail.com>
2017-04-07 17:46:20 +0800

    ADD: DrawerNavigation和StackNavigation

--- app/index.js
+++ app/index.js
@@ -5,16 +5,37 @@ import {
   View,
   ListView,
   Button,
-  TouchableHighlight
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native'
 import { getCnodeTopics } from '../api'
-import { StackNavigator } from 'react-navigation'
+import { StackNavigator, DrawerNavigator } from 'react-navigation'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
 
-class HomeScreen extends React.Component {
+class Home extends React.Component {
   static navigationOptions = {
-    title: 'Home',
+    drawer: () => ({
+      label: '首页',
+      icon: ({ tintColor }) => (
+        <Text>2</Text>
+      ),
+    }),
+    header: ({ navigate }) => {
+      title = '首页';
+      left = (
+        <TouchableOpacity onPress={() => navigate('DrawerOpen') }>
+          <Image
+            source={{ uri: 'https://cdn0.iconfinder.com/data/icons/ui-glyph/100/burger_menu-256.png' }}
+            style={{ width: 30, height: 30, marginLeft: 10 }}
+          />
+        </TouchableOpacity>
+      )
+
+      return { title, left };
+    }
   };
   constructor(props) {
     super(props)
@@ -41,7 +62,9 @@ class HomeScreen extends React.Component {
   _renderRow(rowData) {
     const { navigate } = this.props.navigation;
     return (
-      <Text onPress={ () => navigate('chat')}>{ rowData.title ? rowData.title : null}</Text>
+      <TouchableHighlight  onPress={() => navigate('Topics', { topic: rowData.title})}>
+        <Text>{ rowData.title ? rowData.title : null}</Text>
+      </TouchableHighlight>
     )
   }
   render() {
@@ -59,23 +82,38 @@ class HomeScreen extends React.Component {
   }
 }
 
-class ChatScreen extends React.Component {
+class Topics extends React.Component {
   static navigationOptions = {
-    title: ({ state }) => `Chat with ${state.params.topic}`,
+    title: ({ state }) => `${state.params.topic}`,
   };
   render() {
     const { params } = this.props.navigation.state
     return (
       <View>
-        <Text>Chat with { params.topic }</Text>
+        <Text>{ params.topic }</Text>
       </View>
     );
   }
 }
 
-const App = StackNavigator({
-  Home: { screen: HomeScreen },
-  Chat: { screen: ChatScreen },
+const HomeStack = StackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    headerMode: 'none'
+  }
+);
+
+const DrawerNav = DrawerNavigator({
+  Home: { screen: HomeStack },
 });
+
+const App = StackNavigator(
+  {
+    Home: { screen: DrawerNav },
+    Topics: { screen: Topics }
+  }
+);
 
 export default App
