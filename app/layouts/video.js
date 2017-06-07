057147e0669e7c77d89254ef213dde4d6370f223
kidbai <ybooty0@gmail.com>
2017-06-07 10:37:50 +0800

    UPDATE:init

---
+++ app/layouts/video.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+
+class Video extends React.Component {
+  componentWillMount() {
+  }
+  render() {
+    return (
+      <View>
+        <Text>Video</Text>
+      </View>
+    )
+  }
+}
+
+export default Video
--- app/nav/index.js
+++ app/nav/index.js
@@ -1,100 +1,11 @@
 import { StackNavigator, DrawerNavigator } from 'react-navigation'
-import Home from '../Home'
-import Good from '../Good'
-import Topic from '../Topic'
-
-// 全部
-const HomeStack = StackNavigator(
-  {
-    All: { screen: Home },
-    Topic: { screen: Topic }
-  },
-  {
-    initialRouteName: 'All',
-    headerMode: 'screen',
-  }
-);
-
-// 精华
-const GoodStack = StackNavigator(
-  {
-    Good: { screen: Home },
-    Topic: { screen: Topic }
-  },
-  {
-    initialRouteName: 'Good',
-    headerMode: 'screen',
-  }
-);
-
-// 问答
-const AskStack = StackNavigator(
-  {
-    Ask: { screen: Home },
-    Topic: { screen: Topic }
-  },
-  {
-    initialRouteName: 'Ask',
-    headerMode: 'screen',
-  }
-);
-
-// 分享
-const ShareStack = StackNavigator(
-  {
-    Share: { screen: Home },
-    Topic: { screen: Topic }
-  },
-  {
-    initialRouteName: 'Share',
-    headerMode: 'screen',
-  }
-);
-
-// 招聘
-const JobStack = StackNavigator(
-  {
-    Job: { screen: Home },
-    Topic: { screen: Topic }
-  },
-  {
-    initialRouteName: 'Job',
-    headerMode: 'screen',
-  }
-);
-
-const Drawer = DrawerNavigator({
-  Drawer: {
-    name: 'Drawer',
-    screen: HomeStack
-  },
-  Good: {
-    name: 'Good',
-    screen: GoodStack
-  },
-  Ask: {
-    name: 'Ask',
-    screen: AskStack
-  },
-  Share: {
-    name: 'Share',
-    screen: ShareStack
-  },
-  Job: {
-    name: 'Job',
-    screen: JobStack
-  }
-}, {
-  initialRouteName: 'Drawer',
-  drawerWidth: 250,
-  drawerPosition: 'left'
-});
+import Video from '../layouts/Video'
 
 const App = StackNavigator(
   {
-    Drawer: {
-      name: 'Drawer',
-      screen: Drawer
+    Video: {
+      name: 'Video',
+      screen: Video
     }
   },
   {
